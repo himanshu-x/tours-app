91656d0e99fac9f190d6941b1d73fdfacac50157
himanshu-x <hy12632@gmail.com>
2022-07-30 23:59:47 +0530

    tours app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from "react"
-import { Routes, Route, Link } from "react-router-dom";
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+  const [tours, setTours] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  function removeTour(id) {
+    // console.log(id);
+    setTours(tours.filter((tour) => tour.id !== id))
+
+
+  }
+
+  function fetchTours() {
+
+
+    fetch(url)
+      .then((data) => data.json())
+      .then((data) => {
+        setTours(data)
+        setLoading(false)
+
+      })
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
   return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="home" />
-        <Route path="about" />
-      </Routes>
-    </div>
-  );
+    <main>
+      {/* send tours here */}
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
-export default App;
+export default App
